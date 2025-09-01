dad980868d3ef497e42c15a8a288a675cda9a9cc
igorpilot <ihorzhurba.air@gmail.com>
2025-09-01 19:14:12 +0200

    create debounced function for SearchBar.tsx

--- src/components/Heroes/SearchBar.tsx
+++ src/components/Heroes/SearchBar.tsx
@@ -1,3 +1,6 @@
+import { useDebounce } from "@/hooks/useSearch";
+import { useEffect, useState } from "react";
+
 export const SearchBar = ({
   value,
   onChange,
@@ -5,12 +8,20 @@ export const SearchBar = ({
   value: string;
   onChange: (v: string) => void;
 }) => {
+  const [inputValue, setInputValue] = useState(value);
+  const debouncedValue = useDebounce(inputValue, 100);
+
+  useEffect(() => {
+    if (debouncedValue !== value) {
+      onChange(debouncedValue);
+    }
+  }, [debouncedValue, value, onChange]);
   return (
     <div className="max-w-md mb-4">
       <input
         type="search"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
         placeholder="Search hero by name..."
         className="w-full border border-gray-700 rounded-2xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400 placeholder-yellow-200 "
       />
